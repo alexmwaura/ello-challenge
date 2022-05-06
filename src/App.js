ee523d29cc805172dc5cb3b00f7d8d388e7a709a
alexmwaura <alexmwaura43@gmail.com>
2022-05-06 21:26:30 +0300

    consume graphql api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+import { useQuery, gql } from '@apollo/client';
+import { useState } from 'react';
 import './App.css';
+import Content from './Content';
+
+const BOOK_QUERY = gql`
+  {
+    book {
+      author
+      pages {
+        content
+        pageIndex
+        tokens {
+          position
+          value
+        }
+      }
+      title
+    }
+  }
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const { data, loading, error } = useQuery(BOOK_QUERY);
+  const [text, setText] = useState();
+
+  if (loading) return <div className='App'> "loading..." </div>;
+  if (error)
+    return (
+      <div className='App'>
+        <pre>{error.message}</pre>
+      </div>
+    );
+  else {
+    return (
+      <div className='App'>
+        <h1 className='author'>{data.book.author}</h1>
+        <h3 className='title'>{data.book.title}</h3>
+        <span>{text}</span>
+        <input id='input' type='text' style={{ display: 'none' }}></input>
+
+        {data.book.pages.map(({ content, pageIndex, tokens }, index) => (
+          <Content
+            setText={setText}
+            key={index}
+            content={content}
+            tokens={tokens}
+            pageIndex={pageIndex}
+          />
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
