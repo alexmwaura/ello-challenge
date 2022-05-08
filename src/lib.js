4fdba86ec57265b81ac9908d791bc4fa28f3a70d
alexmwaura <alexmwaura43@gmail.com>
2022-05-08 17:59:04 +0300

    update

--- src/lib.js
+++ src/lib.js
@@ -1,17 +1,17 @@
 export function handlePages(pages) {
-    const newArr = [];
-    let len = pages.length - 1;
-    if (len % 2 !== 0) {
-      len++;
-    }
-    for (let page of pages) {
-      for (let i = 0; i <= page.pageIndex; i++) {
-        if (page.pageIndex === i) {
-          if (i % 2 === 0) {
-            newArr.push([pages[i], pages[page.pageIndex + 1]]);
-          }
+  const newArr = [];
+  let len = pages.length - 1;
+  if (len % 2 !== 0) {
+    len++;
+  }
+  for (let page of pages) {
+    for (let i = 0; i <= page.pageIndex; i++) {
+      if (page.pageIndex === i) {
+        if (i % 2 === 0) {
+          newArr.push([pages[i], pages[page.pageIndex + 1]]);
         }
       }
     }
-    return newArr;
   }
+  return newArr;
+}
