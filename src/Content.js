0e55430c70e6a2ffacb952cf7c853a97d5b6fcaa
alexmwaura <alexmwaura43@gmail.com>
2022-05-08 17:58:17 +0300

    refactor

--- src/Content.js
+++ src/Content.js
@@ -10,16 +10,15 @@ export default function Content(props) {
       t = document.all
         ? document.selection.createRange().text
         : document.getSelection().getRangeAt(0);
-      document.getElementById('input').value = t;
       props.setPosition([t.startOffset, t.endOffset]);
-      props.setText(document.getElementById('input').value);
     }
   });
   return (
     <div id='wrapper'>
       <div id='container'>
-        <h1>Double Click on Word or Highlight</h1>
-        <input id='input' type='text' style={{ display: 'none' }}></input>
+      <h1 style={{ textAlign: 'center', marginTop: '2rem', padding: '2px' }}>
+        Double Click on Word or Highlight
+      </h1>
         <section className='open-book'>
           <header>
             <h1>{props.title}</h1>
@@ -27,11 +26,11 @@ export default function Content(props) {
           </header>
           <article className='article'>
             <div className='page'>
-              <p className='content'>{props.page[0].content}</p>
+              <p className={`content`}>{props.page[0].content}</p>
             </div>
             <br />
             <div className='page'>
-              <p className='content'>{props.page[1].content}</p>
+              <p className={`content`}>{props.page[1].content}</p>
             </div>
           </article>
           <footer>
