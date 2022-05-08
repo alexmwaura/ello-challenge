0ccc0cb321d9a95169339813fe7a0ea4f3d6cfcf
alexmwaura <alexmwaura43@gmail.com>
2022-05-08 17:58:02 +0300

    handle token

--- src/App.js
+++ src/App.js
@@ -1,74 +1,76 @@
-/* eslint-disable no-unused-vars */
-import { useQuery, gql } from '@apollo/client';
+import { useQuery } from '@apollo/client';
 import { useState, useEffect } from 'react';
 import './App.css';
 import Content from './Content';
 import Footer from './Footer';
 import { handlePages } from './lib';
-
-const BOOK_QUERY = gql`
-  {
-    book {
-      author
-      pages {
-        content
-        pageIndex
-        tokens {
-          position
-          value
-        }
-      }
-      title
-    }
-  }
-`;
+import Loader from './Loader';
+import Token from './Token';
+import { BOOK_QUERY } from './graphql';
 
 function App() {
   const { data, loading, error } = useQuery(BOOK_QUERY);
-  const [text, setText] = useState();
   const [index, setIndex] = useState(0);
   const [position, setPosition] = useState([]);
   const [pagArr, setPagArr] = useState([]);
+  const [word, setWord] = useState();
 
+  console.log(pagArr[index]);
   useEffect(() => {
     if (pagArr.length <= 0 && !loading && !error) {
       setPagArr(handlePages(data.book.pages));
     }
-  }, [data, error, loading, pagArr]);
+    if (position.length > 1) {
+      let res = [];
+      pagArr[index].map((pos) =>
+        // eslint-disable-next-line array-callback-return
+        pos.tokens.filter((tok) => {
+          if (tok.position.join('') === position.join('')) {
+            res.push(tok.value);
+            setWord(res[0]);
+          }
+        })
+      );
+    }
+  }, [data, error, index, loading, pagArr, position]);
 
-  function handleNextPage(pageIndex) {
+  function handleNextPage() {
     if (index < pagArr.length - 1) {
       setIndex(index + 1);
     }
   }
 
-  function handlePreviousPage(pageIndex) {
+  function handlePreviousPage() {
     if (index > 0) {
       setIndex(index - 1);
     }
   }
 
-  if (loading)
-    return (
-      <div id='wrapper'>
-        <div id='container'>
-          <section className='open-book'> "loading..."</section>
-        </div>
-      </div>
-    );
+  if (loading) return <Loader />;
+
   if (error)
     return (
       <div className='container open-book'>
         <pre>{error.message}</pre>
       </div>
     );
-  else if (pagArr.length > 0) {
+  if (word) {
+    return (
+      <Token
+        setWord={setWord}
+        word={word}
+        title={data.book.title}
+        author={data.book.author}
+      />
+    );
+  } else if (pagArr.length > 0) {
     return (
       <>
         <Content
           title={data.book.title}
           author={data.book.author}
           page={pagArr[index]}
+          setPosition={setPosition}
         />
         <Footer
           handlePreviousPage={handlePreviousPage}
