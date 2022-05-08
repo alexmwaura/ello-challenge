872de9597be5b555cbdde850b4d39868cc5546f8
alexmwaura <alexmwaura43@gmail.com>
2022-05-08 17:59:12 +0300

    token component

---
+++ src/Token.js
@@ -0,0 +1,32 @@
+export default function Token({ word, setWord, title, author }) {
+  return (
+    <div id='wrapper'>
+      <div id='container'>
+        <h1>Double Click on Word or Highlight</h1>
+        <input id='input' type='text' style={{ display: 'none' }}></input>
+        <section className='open-book'>
+          <header>
+            <h1>{title}</h1>
+            <h6>{author}</h6>
+          </header>
+          <article className='article'>
+            <div className='page'>
+              <div className='text-effect'>
+                <h1 data-text={word}>{word}</h1>
+              </div>
+            </div>
+            <br />
+            <div className='page'></div>
+          </article>
+          <footer>
+            <ol id='page-numbers'>
+              <button className='button-18' onClick={() => setWord(null)}>
+                Done
+              </button>
+            </ol>
+          </footer>
+        </section>
+      </div>
+    </div>
+  );
+}
