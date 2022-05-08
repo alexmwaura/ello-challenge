49b22f3d26e9481c02073c6bcd61271e732d96ff
alexmwaura <alexmwaura43@gmail.com>
2022-05-08 17:58:35 +0300

    handle begining and end of page

--- src/Footer.js
+++ src/Footer.js
@@ -2,18 +2,23 @@ export default function Footer(props) {
   return (
     <div className='footer'>
       <div className='nav_buttons'>
-        <div
-          className='circle ps_prev'
-          onClick={() => props.handlePreviousPage(props.pagArr[props.index])}
-        >
-          <span className='material-symbols-outlined'>arrow_back_ios_new</span>
-        </div>
-        <div
-          className='circle ps_next'
-          onClick={() => props.handleNextPage(props.pagArr[props.index])}
-        >
-          <span className='material-symbols-outlined'>arrow_forward_ios</span>
-        </div>
+        {props.index === 0 ? (
+          <div className='ps_prev'></div>
+        ) : (
+          <div className='ps_prev' onClick={() => props.handlePreviousPage()}>
+            <span className='material-symbols-outlined'>
+              arrow_back_ios_new
+            </span>
+            
+          </div>
+        )}
+        {props.index === props.pagArr.length - 1 ? (
+          <div className='ps_next'></div>
+        ) : (
+          <div className='ps_next' onClick={() => props.handleNextPage()}>
+            <span className='material-symbols-outlined'>arrow_forward_ios</span>           
+          </div>
+        )}
       </div>
     </div>
   );
